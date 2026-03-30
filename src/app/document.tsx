452b8bcc24a5b9df62d995ca114be4b22288ed08
rklpoi5678 <rklpoi5678@naver.com>
2026-03-30 12:33:58 +0900

    fix: wrong types crossOrigin

--- src/app/document.tsx
+++ src/app/document.tsx
@@ -9,7 +9,7 @@ export const Document: React.FC<{ children: React.ReactNode }> = ({ children }) 
       <link rel="modulepreload" href="/src/client.tsx" />
       <link rel="stylesheet" href={styles} />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       <link
         href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&family=Noto+Sans+KR:wght@300;400;500;600;700&family=JetBrains+Mono:wght@400;500;600&display=swap"
         rel="stylesheet"
